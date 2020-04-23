ed1dc97b6931b5ae33806e42f4978e4d10f7f6c0
Enrique Mejia <mejiaej@gmail.com>
2020-04-23 17:58:08 -0500

    fix

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 require("dotenv").config();
 const path = require("path");
 const express = require("express");
-const http = require("http");
 const app = express();
-
-const socket = require("socket.io");
 
 if (process.env.PROD) {
   app.use(express.static(path.join(__dirname, './client/build')));
@@ -15,31 +12,5 @@ if (process.env.PROD) {
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`app is listening on port ${port}`));
-
-const server = http.createServer(app);
-const io = socket(server);
-
-const users = {};
-
-io.on('connection', socket => {
-    if (!users[socket.id]) {
-        users[socket.id] = socket.id;
-    }
-    socket.emit("yourID", socket.id);
-    io.sockets.emit("allUsers", users);
-    socket.on('disconnect', () => {
-        delete users[socket.id];
-    })
-
-    socket.on("callUser", (data) => {
-        io.to(data.userToCall).emit('hey', {signal: data.signalData, from: data.from});
-    })
-
-    socket.on("acceptCall", (data) => {
-        io.to(data.to).emit('callAccepted', data.signal);
-    })
-});
-
-server.listen(8000, () => console.log('server is running on port 8000'));
 
 
